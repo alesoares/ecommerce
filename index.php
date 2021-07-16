f680dacd67e4a2d493eea2e13a75e1a8c26da8e2
Alessandro Soares <alesoares1975@gmail.com>
2021-07-16 00:46:13 -0300

    criamos os CRUD

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ use \Hcode\PageAdmin;/*SÃO NAMESPACE; OU SEJA DENTRO DO VENDOR TENHO DEZENAS DE
 
 use \Hcode\Model\User;/*SÃO NAMESPACE; OU SEJA DENTRO DO VENDOR TENHO DEZENAS DE CLASSES. QUAL EU QUERO???*/
 
+/*ARQUIVOS DE ROTAS PARA O "SEO"; OU MOTORES DE BUSCA EM SITES*/
+
 $app = new \Slim\Slim();/*POR CAUSA DAS ROTAS, PARA FACILITAR, O "SEO" AGORA BUSCA POR ROTAS, POR CONTA DE RANQUEAMENTO DE BUSCA*/
 
 $app->config('debug', true);
@@ -62,6 +64,97 @@ $app->get('/admin/logout', function() {
 	header( "Location: /admin/login");
 
 	exit;
+});
+
+$app->get('/admin/users', function(){
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl( "users", array(
+		"users"=>$users
+	));
+});
+
+$app->get('/admin/users/create', function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl( "users-create" );
+});
+
+
+
+$app->get('/admin/users/:iduser/delete', function( $iduser ){
+
+	User::verifyLogin();
+
+	$user = new User();
+ 
+    $user->get((int)$iduser);
+
+    $user->delete();
+
+    header("Location: /admin/users");
+	exit();
+
+});
+
+
+
+$app->get('/admin/users/:iduser', function( $iduser ){
+
+	User::verifyLogin();
+
+	$user = new User();
+ 
+    $user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl( "users-update", array(
+		"user"=>$user->getValues()
+	));
+});
+
+$app->post('/admin/users/create', function(){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST['inadmin'] = (isset( $_POST['inadmin']))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit();
+});
+
+$app->post('/admin/users/:iduser', function( $iduser ){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST['inadmin'] = (isset( $_POST['inadmin']))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit();
+
 });
 
 $app->run();/*RESPOSÁVEL POR LIGAR TUDO NO SITE*/
